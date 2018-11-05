475d1172238f88b2344c7ff33098204a5bdbc58c
noliaki <noliaki@gmail.com>
2018-11-05 13:51:12 +0900

    [wip]

--- src/entry.ts
+++ src/entry.ts
@@ -3,6 +3,10 @@ import * as THREE from 'three'
 import perlinNoise from './perlin-noise.fs'
 import fragmentShader from './fragment-shader.fs'
 
+const rangeR: HTMLInputElement = document.getElementById('range_r') as HTMLInputElement
+const rangeG: HTMLInputElement = document.getElementById('range_g') as HTMLInputElement
+const rangeB: HTMLInputElement = document.getElementById('range_b') as HTMLInputElement
+
 const container: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement
 
 const camera: THREE.Camera = new THREE.Camera()
@@ -14,7 +18,8 @@ const uniforms = {
   u_time: { type: 'f', value: 100.0 },
   u_resolution: { type: 'v2', value: new THREE.Vector2() },
   u_mouse: { type: 'v2', value: new THREE.Vector2() },
-  c_mouse: { type: 'v2', value: new THREE.Vector2() }
+  c_mouse: { type: 'v2', value: new THREE.Vector2() },
+  u_rgbOffset: { type: 'v3', value: new THREE.Vector3(0.2, 0.1, 0.0) }
 }
 
 const material: THREE.ShaderMaterial = new THREE.ShaderMaterial({
@@ -46,6 +51,19 @@ document.addEventListener('mousemove', (event: MouseEvent): void => {
 scene.add(mesh)
 setSize()
 animate()
+
+rangeR.addEventListener('change', (event: Event): void => {
+  console.log((event.target as HTMLInputElement).value)
+  uniforms.u_rgbOffset.value.x = +(event.target as HTMLInputElement).value
+}, false)
+rangeG.addEventListener('change', (event: Event): void => {
+  console.log((event.target as HTMLInputElement).value)
+  uniforms.u_rgbOffset.value.y = +(event.target as HTMLInputElement).value
+}, false)
+rangeB.addEventListener('change', (event: Event): void => {
+  console.log((event.target as HTMLInputElement).value)
+  uniforms.u_rgbOffset.value.z = +(event.target as HTMLInputElement).value
+}, false)
 
 function setSize (): void {
   container.width = window.innerWidth
--- src/fragment-shader.fs
+++ src/fragment-shader.fs
@@ -3,6 +3,7 @@ uniform float u_time; // time
 uniform vec2  u_resolution; // resolution
 uniform vec2 u_mouse;
 uniform vec2 c_mouse;
+uniform vec3 u_rgbOffset;
 
 void main(void){
   vec2 p = (gl_FragCoord.xy * 2.0 - u_resolution) / min(u_resolution.x, u_resolution.y);
@@ -13,9 +14,9 @@ void main(void){
   float offset = (1.0 / length(vec2(p.x - p_mouse.x, p.y + p_mouse.y))) * noise / 2.0;
 
   vec3 rgb_color = vec3(
-    (snoise(vec3(p.x / 3.0, p.y / 3.0, u_time / 1.1)) + 1.0) / 2.0 + 0.2 + offset,
-    (snoise(vec3(p.x / 2.0, p.y / 2.0, u_time / 1.2)) + 1.0) / 2.0 + 0.1 + offset,
-    (snoise(vec3(p.x / 5.0, p.y / 5.0, u_time / 1.3)) + 1.0) / 2.0 + offset
+    (snoise(vec3(p.x / 3.0, p.y / 3.0, u_time / 1.1)) + 1.0) / 2.0 + u_rgbOffset.x + offset,
+    (snoise(vec3(p.x / 2.0, p.y / 2.0, u_time / 1.2)) + 1.0) / 2.0 + u_rgbOffset.y + offset,
+    (snoise(vec3(p.x / 5.0, p.y / 5.0, u_time / 1.3)) + 1.0) / 2.0 + u_rgbOffset.z + offset
   );
 
   gl_FragColor = vec4(rgb_color, 1.0);
